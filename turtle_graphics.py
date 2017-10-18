8cc96a695a9ab29673bd7eb55cef76c9d51bd75f
Cano <mcano4216@greenville.k12.sc.us>
2017-10-18 09:32:40 -0400

    first commit

--- turtle_graphics.py
+++ turtle_graphics.py
@@ -1,21 +1,35 @@
 from turtle import *
 
-def draw_face(x, y, points, line, fill):
+def draw_pupil(x, y, points, line, fill):
     penup()
     goto(x,y)
     pendown()
 
+    turn = 180 - (360 / points)
+
+    radius = 50
+
     color(line, fill)
 
+
+    circle(50)
+    
     for i in range(points):
-        fd(100)
-        rt(45)
+        penup()
+        lt(90)
         fd(50)
-        lt(60)
+        pendown()
+        
+        rt(180)
+        fd(50)
+        
+    end_fill()
+
+        
+
+draw_pupil(0, 0, 36, "black", "blue")
 
 
-draw_face(0, 0, 1, "black", "blue")
-
-go(0)
+goto(0)
 
 done()
